b8ae4d92a9fde46e33a61683df9422a730549ea7
Kathy Rodante <kathy.rodante@gmail.com>
2020-04-20 15:30:24 -0700

    Use production config from Heroku ENV variables and decrease socket timeout

--- lib/ac_catalog_web/endpoint.ex
+++ lib/ac_catalog_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule AcCatalogWeb.Endpoint do
   ]
 
   socket "/socket", AcCatalogWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.

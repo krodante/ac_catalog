fcba8cea715a43e93918d437205c3a4f221b50f1
Kathy Rodante <kathy.rodante@gmail.com>
2020-04-20 17:06:46 -0700

    change prod app url

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :ac_catalog, AcCatalogWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "thawing-peak-67329.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "ac-catalog.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
